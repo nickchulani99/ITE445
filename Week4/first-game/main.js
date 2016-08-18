d800069cea783fa0a30561f06cc98a60ff1a97f5
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-08-18 09:57:39 +0700

    Player Movement

--- Week4/first-game/main.js
+++ Week4/first-game/main.js
@@ -15,10 +15,28 @@ var mainState = {
 		game.physics.arcade.enable(this.player);
 
 		this.player.body.gravity.y = 500;
+
+		this.cursor = game.input.keyboard.createCursorKeys();
 	},
 
 	update: function() {
+		this.movePlayer();
+	},
 
+	movePlayer: function() {
+		if (this.cursor.left.isDown) {
+			this.player.body.velocity.x = -200;
+		}
+		else if (this.cursor.right.isDown) {
+			this.player.body.velocity.x = 200;
+		}
+		else {
+			this.player.body.velocity.x = 0;
+		}
+
+		if (this.cursor.up.isDown && this.player.body.touching.down) {
+			this.player.body.velocity.y = -320;
+		}
 	},
 };
 	
