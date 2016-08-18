e997381a33bb0a65daf7713da89788e4f6d46ca2
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-08-18 09:09:26 +0700

    main.js content

---
+++ Week4/first-game/main.js
@@ -0,0 +1,19 @@
+var mainState = {
+	
+	preload: function() {
+
+	},
+
+	create: function() {
+
+	},
+
+	update: function() {
+
+	},
+};
+	
+var game = new Phaser.Game(500,340,Phaser.AUTO, 'gameDiv');
+
+game.state.add('main', mainState);
+game.state.start('main');
